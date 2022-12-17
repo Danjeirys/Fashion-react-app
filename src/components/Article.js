1bf0f1d449e08e9644292e3f53411ef055fd62cd
Jerry Martinez <jaysavageg@gmail.com>
2022-12-16 19:26:17 -0500

    done

---
+++ src/components/Article.js
@@ -0,0 +1,27 @@
+export default function Article () {
+    return (
+        <main>
+            <article>
+            <article>
+            <span className="date">11/12/22</span>
+            <h3>On the Street in Brooklyn</h3>
+            <img src="./images/blog-image-1.jpeg" alt="images-1"/>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit perferendis corrupti numquam minus eos odio quia similique! Repellat, incidunt. Distinctio obcaecati incidunt ea sint commodi veniam doloribus minus quas inventore?</p>
+            <div>
+                <a className='continue' href="#">Continue...</a>
+            </div>
+        </article>
+
+        <article>
+            <span className="date">11/11/20</span>
+            <h3>Vintage in Vogue</h3>
+            <img src="./images/blog-image-2.jpeg" alt="images-2"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quos, id officiis sequi facere maiores velit molestiae obcaecati modi sed laudantium, accusamus labore omnis fugiat magni repellendus dolor perferendis nisi.</p>
+            <div>
+                <a className='continue' href="#">Continue...</a>
+            </div>
+        </article>
+            </article>
+        </main>
+    )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer () {
+    return (
+        <footer>
+        <nav aria-label="Main Navigation" role="navigation">
+            <ul>
+                <li><a href="women">Women's</a></li>
+                <li><a href="men">Men's</a></li>
+                <li><a href="street">On the Street</a></li>
+                <li><a href="catwalk">The Catwalk</a></li>
+                <li><a href="adwatch">AdWatch</a></li>
+                <li><a href="about">About</a></li>
+            </ul>
+        </nav>
+        <span className="copyright">&copy; 2022 Valet Industries, Inc.</span>
+    </footer>
+    )
+}
